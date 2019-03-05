a6e48fc1cb7268c7e52b16197a8fec6570599e0d
Dominik Rosser <31032638+dominikrosser@users.noreply.github.com>
2019-03-05 21:19:52 +0100

    Remove some unused imports

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,6 +1,4 @@
 #![feature(custom_attribute)]   // Enable custom attributes like 'table_name'
-//#![feature(plugin)]           //
-//#![feature(proc_macro_derive)]  // Lets us apply procedural macros
 #![feature(const_fn)]           // Allows us to define inline functions
 #![feature(proc_macro_hygiene)] // TODO: This ist for rocket but what does this macro do?
 #![feature(decl_macro)]         // Allows us to create declarative macros
@@ -9,11 +7,8 @@
 
 #[macro_use]                    //TODO: Remove imports, import with use only since rust 2018
 extern crate diesel;            //TODO: Remove imports, import with use only since rust 2018
-//extern crate r2d2;
-//extern crate r2d2_diesel;
 #[macro_use]
 extern crate rocket;            //TODO: Remove imports, import with use only since rust 2018
-//extern crate rocket_contrib;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
@@ -23,12 +18,8 @@ mod models;         // Contains the db models and functionality to read, update,
 mod db;             // Contains helpers for the database connection pool 
 mod static_files;   // Containes api routes for serving static files
 
-//use ::diesel::prelude::*;
-//use ::diesel::pg::PgConnection;
 use ::dotenv::dotenv;
 use ::std::env;
-//use ::rocket;
-//use ::diesel;
 
 fn init_rocket() -> rocket::Rocket {
     dotenv().ok();
