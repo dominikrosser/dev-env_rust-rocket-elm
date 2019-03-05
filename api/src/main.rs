a9c614ea0bb44c40e5f08978008dc85a94008397
Dominik Rosser <dominik.rosser@protonmail.ch>
2019-03-05 20:08:31 +0100

    Add r2d2 and serde dependencies for db conn pool

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,8 +1,21 @@
 #![feature(custom_attribute)]   // Enable custom attributes like 'table_name'
+//#![feature(plugin)]             //
+#![feature(custom_derive)]      // Lets us apply procedural macros
+#![feature(const_fn)]           // Allows us to define inline functions
+#![feature(decl_macro)]         // Allows us to create declarative macros
 #![allow(dead_code)]            // Disable warning for dead code
+
 
 #[macro_use]                    //TODO: Remove imports, import with use only since rust 2018
 extern crate diesel;            //TODO: Remove imports, import with use only since rust 2018
+//extern crate r2d2;
+//extern crate r2d2_diesel;
+//extern crate rocket;
+//extern crate rocket_contrib;
+//#[macro_use]
+//extern crate serde_derive;
+//#[macro_use]
+//extern crate serde_json;
 
 mod schema;// Contains the db schema of diesel
 mod models;// Contains the db models and functionality to read, update, insert
