81407bd7c1bf8d453a793577f373865ae5fd2d24
Dominik Rosser <dominik.rosser@protonmail.ch>
2019-03-08 21:03:43 +0100

    Fix api routing module import errors and update the code to work with new rocket_contrib::json::JsonValue

--- api/src/routes.rs
+++ api/src/routes.rs
@@ -1,55 +1,56 @@
 use crate::db::Conn as DbConn;
 use crate::models::{Post, NewPost};
 use ::rocket_contrib::json::Json;
+use ::rocket_contrib::json::JsonValue;
 use ::serde_json::Value;
 
 #[get("/posts", format = "application/json")]
-fn index(conn: DbConn) -> Json<Value> {
+pub fn index(conn: DbConn) -> JsonValue {
     let posts = Post::all(&conn);
 
-    Json(json!({
+    json!({
         "status": 200,
         "result": posts,
-    }))
+    })
 }
 
 #[post("/posts", format= "application/json", data = "<new_post>")]
-fn new(conn: DbConn, new_post: Json<NewPost>) -> Json<Value> {
-    Json(json!({
+pub fn new(conn: DbConn, new_post: Json<NewPost>) -> JsonValue {
+    json!({
         "status": Post::insert(new_post.into_inner(), &conn),
         "result": Post::all(&conn).first(),
-    }))
+    })
 }
 
 #[get("/posts/<id>", format = "application/json")]
-fn show(conn: DbConn, id: i32) -> Json<Value> {
+pub fn show(conn: DbConn, id: i32) -> JsonValue {
    let result = Post::get_by_id(id, &conn);
    let status = if result.is_empty() { 404 } else { 200 };
 
-   Json(json!({
+   json!({
        "status": status,
        "result": result.get(0),
-    }))
+    })
 }
 
 #[put("/posts/<id>", format = "application/json", data = "<post>")]
-fn update(conn: DbConn, id: i32, post: Json<NewPost>) -> Json<Value> {
+pub fn update(conn: DbConn, id: i32, post: Json<NewPost>) -> JsonValue {
     let status = if Post::update_by_id(id, &conn, post.into_inner()) { 200 } else { 404 };
 
-    Json(json!({
+    json!({
         "status": status,
         "result": null,
-    }))
+    })
 }
 
 #[delete("/posts/<id>")]
-fn delete(id: i32, conn: DbConn) -> Json<Value> {
+pub fn delete(id: i32, conn: DbConn) -> JsonValue {
     let status = if Post::delete_by_id(id, &conn) { 200 } else { 404 };
 
-    Json(json!({
+    json!({
         "status": status,
         "result": null,
-    }))
+    })
 }
 
 /** Sample get with select */
@@ -62,9 +63,9 @@ fn delete(id: i32, conn: DbConn) -> Json<Value> {
 //}
 
 #[catch(404)]
-fn not_found() -> Json<Value> {
-    Json(json!({
+pub fn not_found() -> JsonValue {
+    json!({
         "status": "error",
         "reason": "Resource was not found",
-    }))
+    })
 }
